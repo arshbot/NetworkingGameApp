79c9d2b54c165031572543b5c79ec28ac566754f
Harsha Goli <harshagoli@gmail.com>
2018-11-26 12:23:02 -0500

    all the shit

--- app/src/main/java/com/example/harshagoli/networkinggame/SwipeContacts.java
+++ app/src/main/java/com/example/harshagoli/networkinggame/SwipeContacts.java
@@ -1,15 +1,64 @@
 package com.example.harshagoli.networkinggame;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class SwipeContacts extends Activity {
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+
+public class SwipeContacts extends AppCompatActivity {
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        this.setContentView(R.layout.swipe_contacts_main);
+//
+//    }
+//
+//}
+//
+//public class MainActivity extends AppCompatActivity {
+
+    private SwipePlaceHolderView mSwipeView;
+    private Context mContext;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.swipe_contacts);
+        setContentView(R.layout.swipe_contacts_main);
 
+        mSwipeView = (SwipePlaceHolderView)findViewById(R.id.swipeView);
+        mContext = getApplicationContext();
+
+        mSwipeView.getBuilder()
+                .setDisplayViewCount(3)
+                .setSwipeDecor(new SwipeDecor()
+                        .setPaddingTop(20)
+                        .setRelativeScale(0.01f)
+                        .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
+                        .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
+
+
+        for(Profile profile : Utils.loadProfiles(this.getApplicationContext())){
+            mSwipeView.addView(new TinderCard(mContext, profile, mSwipeView));
+        }
+
+        findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(false);
+            }
+        });
+
+        findViewById(R.id.acceptBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(true);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/harshagoli/networkinggame/TinderCard.java
@@ -0,0 +1,65 @@
+package com.example.harshagoli.networkinggame;
+
+import android.widget.ImageView;
+
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.View;
+
+import static com.example.harshagoli.networkinggame.TinderCard.R;
+
+@Layout(R.layout.tinder_card_view)
+public class TinderCard {
+
+    public static final android.R.attr R = ;
+    @View(R.id.profileImageView)
+    private ImageView profileImageView;
+
+    @View(R.id.nameAgeTxt)
+    private TextView nameAgeTxt;
+
+    @View(R.id.locationNameTxt)
+    private TextView locationNameTxt;
+
+    private Profile mProfile;
+    private Context mContext;
+    private SwipePlaceHolderView mSwipeView;
+
+    public TinderCard(Context context, Profile profile, SwipePlaceHolderView swipeView) {
+        mContext = context;
+        mProfile = profile;
+        mSwipeView = swipeView;
+    }
+
+    @Resolve
+    private void onResolved(){
+        Glide.with(mContext).load(mProfile.getImageUrl()).into(profileImageView);
+        nameAgeTxt.setText(mProfile.getName() + ", " + mProfile.getAge());
+        locationNameTxt.setText(mProfile.getLocation());
+    }
+
+    @SwipeOut
+    private void onSwipedOut(){
+        Log.d("EVENT", "onSwipedOut");
+        mSwipeView.addView(this);
+    }
+
+    @SwipeCancelState
+    private void onSwipeCancelState(){
+        Log.d("EVENT", "onSwipeCancelState");
+    }
+
+    @SwipeIn
+    private void onSwipeIn(){
+        Log.d("EVENT", "onSwipedIn");
+    }
+
+    @SwipeInState
+    private void onSwipeInState(){
+        Log.d("EVENT", "onSwipeInState");
+    }
+
+    @SwipeOutState
+    private void onSwipeOutState(){
+        Log.d("EVENT", "onSwipeOutState");
+    }
+}
